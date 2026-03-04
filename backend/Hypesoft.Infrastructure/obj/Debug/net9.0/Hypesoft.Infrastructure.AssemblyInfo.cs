f76388a3c2c9f67e75ec4e7831db5e95216b32ca
Alexssander Ferreira de Almeida <alexssander.f.almeida2006@email.com>
2026-03-04 08:07:18 -0300

    feat: complete inventory management API

--- backend/Hypesoft.Infrastructure/obj/Debug/net9.0/Hypesoft.Infrastructure.AssemblyInfo.cs
+++ backend/Hypesoft.Infrastructure/obj/Debug/net9.0/Hypesoft.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hypesoft.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b38e62947c987d2addfa15db6ac316a8429a217")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c852ee54aa105f80b321362e0006cb55f88062e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hypesoft.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hypesoft.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
